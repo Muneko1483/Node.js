69f703d76db707c0d2282a5c78cd3cf810a4c11d
Guzman, Juan <juan.guzman@accenture.com>
2018-02-21 14:36:10 -0600

    Continua 8

---
+++ nodejs/08ApiRESTMongo/controllers/movie-controller.js
@@ -0,0 +1,75 @@
+'use strict'
+
+let 
+    MovieModel = require('../models/movie-model'),
+    MovieController = () => {
+
+    } 
+
+MovieController.getAll = (req, res, next) =>{
+
+    MovieModel.getAll((docs) =>{
+        let locals = {
+            title : 'Lista de Peliculas',
+            data : docs
+        }
+         res.render('index', locals)   
+         
+    })
+}
+MovieController.getOne = (req, res, next) =>{
+    let movie_id = req.params.movie_id
+    console.log(movie_id)
+
+    MovieModel.getOne(movie_id,(docs) => {
+           let locals ={
+               title : 'Editar Pelicula',
+               data : docs
+           }
+           res.render('edit-movie', locals)
+
+
+     })
+}
+
+
+MovieController.save = (req, res, next) =>{
+    let movie = {
+        movie_id : req.body.movie_id,
+        title : req.body.title,
+        release_year : req.body.release_year,
+        rating : req.body.rating,
+        image : req.body.image
+    }
+    console.log(movie)
+
+    MovieModel.save(movie, () => res.redirect('/'))
+}
+
+MovieController.delete = (req, res, next) =>{
+    let movie_id = req.params.movie_id
+    console.log(movie_id)
+
+    MovieModel.delete(movie_id,(err, rows) => res.redirect('/') )
+
+}
+
+MovieController.addForm = (req, res, next) => res.render('add-movie', {title: 'Agregar Pelicula'})
+
+MovieController.error404 = (req, res, next) =>{
+    let error = new Error(),
+          locals = {
+              title : 'Error 404',
+              description : 'Recurso No Encontrado',
+              error : error
+          }
+
+      error.status = 404
+          
+      res.render('error',locals)    
+
+      next()
+
+}
+
+module.exports = MovieController 
---
+++ nodejs/08ApiRESTMongo/models/movie-connection.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let
+    mongoose = require('mongoose'),
+    conf = require('./db-conf'),
+    Schema = mongoose.Schema,
+    MovieSchema = new Schema({
+
+        movie_id : "string",
+        title : "string",
+        release_year : "string",
+        rating : "string",
+        image : "string"
+
+    },
+    {
+        collection : "movie"
+    }), 
+        MovieModel = mongoose.model("Movie", MovieSchema)
+
+mongoose.connect(`mongodb:\/\/${conf.mongo.host}/${conf.mongo.db}`)
+      
+module.exports = MovieModel
